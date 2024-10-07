4e201b125861c46a769c52d693138c2d0bf57cbd
Nitin Bhardwaj <godfu2000@gmail.com>
2024-10-07 23:47:27 +0530

    applied changes

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,11 +2,11 @@ import pytest
 from utils.api_helpers import *
 
 @pytest.mark.smoke
-def test_generate_token():
+def test_generate_token_success():
     endpoint = "/auth"
     payload = {"username": "admin", "password": "password123"}
     headers = {"Content-Type": "application/json"}
-    res = get_token(endpoint, payload, headers)
+    res = post(endpoint, payload, headers)
     assert res.status_code == 200
     assert res.json()["token"] is not None
 
@@ -16,6 +16,6 @@ def test_generate_token_failure():
     endpoint = "/auth"
     payload = {"username": "invaliduser", "password": "wrongpass"}
     headers = {"Content-Type": "application/json"}
-    res = get_token(endpoint, payload, headers)
+    res = post(endpoint, payload, headers)
     assert res.status_code == 200
     assert res.json()["reason"] == "Bad credentials"
